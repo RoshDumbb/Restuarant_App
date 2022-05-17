1a297df5b663e6d7fec341edfac71f13ee3f771e
RoshDumbb <roshanidumbhare30@gmail.com>
2022-05-17 13:51:30 +0530

    some api changes

---
+++ searchforhotel/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Google from './Components/Google';
+
+function App() {
+  return (
+    <div className="App">
+     <Google/>
+    </div>
+  );
+}
+
+export default App;
